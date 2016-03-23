528a26723f3ed5abcc05305110d2144a0ae79934
thanujalk <jatljayasinghe@gmail.com>
2016-03-23 10:21:17 +0530

    Updating dependencies

--- component/org.wso2.carbon.security/src/main/java/org/wso2/carbon/security/jaas/CarbonJAASConfiguration.java
+++ component/org.wso2.carbon.security/src/main/java/org/wso2/carbon/security/jaas/CarbonJAASConfiguration.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.security.jaas;
 
 import org.wso2.carbon.security.boot.ProxyLoginModule;
 import org.wso2.carbon.security.internal.CarbonSecurityDataHolder;
+import sun.security.provider.ConfigFile.Spi;
 
 import javax.security.auth.login.AppConfigurationEntry;
 import javax.security.auth.login.Configuration;
@@ -12,12 +29,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This contains the carbon implementation of the Configuration class
+ */
 public class CarbonJAASConfiguration extends Configuration {
 
-    private final sun.security.provider.ConfigFile.Spi spi;
+    private final Spi spi;
 
     public CarbonJAASConfiguration() {
-        spi = new sun.security.provider.ConfigFile.Spi();
+        spi = new Spi();
     }
 
     public void init() {
@@ -25,7 +45,7 @@ public class CarbonJAASConfiguration extends Configuration {
     }
 
     public CarbonJAASConfiguration(URI uri) {
-        spi = new sun.security.provider.ConfigFile.Spi(uri);
+        spi = new Spi(uri);
     }
 
     @Override
