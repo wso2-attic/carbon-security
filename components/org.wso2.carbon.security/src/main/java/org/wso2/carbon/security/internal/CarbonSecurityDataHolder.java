c152836a856fb744ed995fc543809c7f13041f15
omindu <omindu@wso2.com>
2016-04-06 22:24:42 +0530

    Fixing checkstyle issues.

--- components/org.wso2.carbon.security/src/main/java/org/wso2/carbon/security/internal/CarbonSecurityDataHolder.java
+++ components/org.wso2.carbon.security/src/main/java/org/wso2/carbon/security/internal/CarbonSecurityDataHolder.java
@@ -105,7 +105,8 @@ public class CarbonSecurityDataHolder {
         return loginModuleMap.get(className);
     }
 
-    public void registerAuthorizationStoreConnector(String key, AuthorizationStoreConnector authorizationStoreConnector) {
+    public void registerAuthorizationStoreConnector(String key, AuthorizationStoreConnector
+            authorizationStoreConnector) {
         authorizationStoreConnectorMap.put(key, authorizationStoreConnector);
     }
 
